c0f692c88d7f90163bc149c8358b505eae5aeb9c
anastasiarom <71031040+anastasiarom@users.noreply.github.com>
2020-12-20 23:14:41 +0300

    Add files via upload

---
+++ files/Tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Tileset2" tilewidth="64" tileheight="64" tilecount="930" columns="30">
+ <image source="Tileset2.png" width="1923" height="2022"/>
+</tileset>
---
+++ files/Tileset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Tileset3" tilewidth="64" tileheight="64" tilecount="133" columns="19">
+ <image source="Tileset3.png" width="1247" height="465"/>
+</tileset>
